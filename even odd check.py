59f663aa5006079dbcd763948e9a1b223ebeac71
surendrasingh121099 <surendrasingh1210992gmail.com>
2025-11-15 09:58:57 +0530

    Initial commit

--- even odd check.py
+++ even odd check.py
@@ -1,3 +1,3 @@
 x = int(input("enter the integer value: "))
-print("even") if x % 2 == 0 else print("odd")
+print("the number is even") if x % 2 == 0 else print("the number is odd")
 # This code checks if the input integer is even or odd and prints the result accordingly.
